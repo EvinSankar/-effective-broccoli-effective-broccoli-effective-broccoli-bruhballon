17b7d1f19bf35baf00530f638a81a1bad01700ff
EvinSankar <101604775+EvinSankar@users.noreply.github.com>
2022-05-18 17:48:54 -0400

    effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli?bruh
     effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli? effective-broccoli?ballonbruhbruh

---
+++ sketch.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world, backgroundImg;
+var cannonball
+var balls=[]
+
+var canvas, angle, tower, ground, cannon;
+var score = 0;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+  angleMode(DEGREES)
+  angle=15
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+
+  
+  var options = {
+    isStatic: true
+  }
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, options);
+  World.add(world, tower);
+
+  cannon = new Cannon(180, 110, 130, 100, angle);
+  
+}
+//ok
+function draw() {
+
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  push();
+  imageMode(CENTER);
+  image(towerImage,tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  cannon.display();
+ for(var i = 0;i < balls.length;i++){
+   showcannonballs(balls[i],i)
+ }
+  
+}
+function keyReleased(){
+  if(keyCode==DOWN_ARROW){
+   balls[balls.length-1].shoot()
+  }
+}
+function showcannonballs(ball,i){
+if(ball){
+ ball.display()
+}
+
+
+
+
+
+
+
+
+}
+function keyPressed(){
+if(keyCode==DOWN_ARROW){
+  var cannonball = new Cannonball (cannon.x,cannon.y)
+  balls.push(cannonball)
+}
+}
